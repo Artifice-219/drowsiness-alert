51456843513b63498c098fe3ae40352ab3793526
Artifice-219 <johnphilliplormalbas@gmail.com>
2025-03-22 06:08:05 -0700

    changed to mediaPipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cv2
-
+import mediapipe as mp
+# initializing mediaPipe face detection
+mp_face_detection = mp.solutions.face_detection
+face_detection = mp_face_detection.FaceDetection(min_detection_confidence = 0.5)
 # Open the default camera
 cam = cv2.VideoCapture(0)
 
@@ -13,13 +16,23 @@ out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
 while True:
     ret, frame = cam.read()
+    if not ret:
+        break
+    # convert frame to rgb for mediaPipe 
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # face detection here
+    results = face_detection.process(rgb_frame)
+    # draw some bounding box if faces are detected
+    if results.detections:
+        for detection in results.detections:
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, _ = frame.shape
+            x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+    cv2.imshow('Face Detection',frame)
     # Write the frame to the output file
     out.write(frame)
-
-    # Display the captured frame
-    cv2.imshow('Camera', frame)
-
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
         break
